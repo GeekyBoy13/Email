54ba94e9b371ad3b03e5b8c10a5967335944c271
GeekyBoy13 <63165477+GeekyBoy13@users.noreply.github.com>
2020-06-14 15:28:23 -0400

    Fixed a glitch

--- public.py
+++ public.py
@@ -18,8 +18,7 @@ def change(x):
     return x
 
 # Get information from user, When asked for message, type STOP in all caps to end the message.
-x= input('What email are you using?  ')
-sender_email = choose(x)
+sender_email= input('What email are you using?  ')
 receiver_email = input('Who are you sending it to?  ')
 password = getpass.getpass(prompt = 'What is your password?  ')
 subject = input('What is the subject?  ')
@@ -33,13 +32,16 @@ while True:
         lines.append(line)
 message = '<br>'.join(lines)
 
-lust = re.findall(r"https?://www[.]\w+[.]\w+/*\S+", message)
+lust = re.findall(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", message)
+lest = []
+for i in lust:
+    lest.append(i[0])
 last = re.findall(r">\s\S+\s<", message)
 
 a = message
-for i, j in zip(lust, last):
-    a = re.sub(change('<' + i + j[1:]), "<a href='{}'>{}</a>".format(i[0:-1], j[2:-2]), a)
-
+for i, j in zip(lest, last):
+    a = re.sub(change(i + j), "<a href='{}'>{}</a>".format(i, j[2:-2]), a)
+    
 message = MIMEText(a, 'html')
 message['Subject'] = subject
 message['From'] = sender_email
